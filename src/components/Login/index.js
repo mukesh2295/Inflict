289793c10037f97e45f18b82a1e3a2326c4044af
sanju <sanju@sanjus-MacBook-Air.local>
2019-06-10 10:53:16 +0530

    first commit

---
+++ src/components/Login/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./style.css";
+import ValidatedLoginForm from "./ValidateForm";
+
+const form = function App() {
+  return (
+    <div className="App">
+      <h1> Login Form</h1>
+      <ValidatedLoginForm />
+    </div>
+  );
+}
+
+export default form;
